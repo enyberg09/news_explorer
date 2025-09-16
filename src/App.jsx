43f3c9915001d6bca8ac8640978878b0645a4d50
Elissa Nyberg <esnyberg08@gmail.com>
2025-09-16 13:44:45 -0400

    React basic  setup for About and Footer

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import About from './components/About/about.jsx'
+import Footer from './components/Footer/footer.jsx'
+
+
+function App() {
+  return (
+    <div>
+      <About />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <div>
+            <p>© 2025 Elissa Nyberg - News Explorer</p>
+            <p>Powered by NewsAPI</p>
+        </div>
+    );
+}
+
+export default Footer;

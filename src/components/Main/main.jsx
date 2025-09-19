54902b6144c3e6af26b7e832e16333ca45a909e0
Elissa Nyberg <esnyberg08@gmail.com>
2025-09-19 14:44:36 -0400

    Main Card styling

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -3,9 +3,15 @@ import "./main.css"
 function Main() {
     return (
         <main className="main">
+            <img 
+                src="../../src/images/main-page-pic.svg" 
+                alt="Main page pic" 
+                className="main__image"
+                />
+                <div className="main__content">
             <h1 className="main__title">What's going on in the world?</h1>
             <p className="main__subtitle">Find the latest news on any topic and save them in your personal account.</p>
-            <form className="main__search-form">
+             <form className="main__search-form">
                 <input
                     className="main__search-input"
                     type="text"
@@ -15,7 +21,8 @@ function Main() {
                         Search
                     </button>
             </form>
-        </main>
+            </div>
+                   </main>
         
     );
 }

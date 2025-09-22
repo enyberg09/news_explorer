0e8b2aba5bb0a6ce3f7f05910d5b26148ba175c3
Elissa Nyberg <esnyberg08@gmail.com>
2025-09-22 09:17:58 -0400

    Initial header styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter } from 'react-router-dom';
 import About from './components/About/about.jsx'
 import Main from './components/Main/main.jsx'
 import Footer from './components/Footer/footer.jsx'
@@ -5,12 +6,14 @@ import Header from './components/Header/header.jsx'
 
 function App() {
   return (
+    <BrowserRouter>
     <div>
       <Header />
       <Main />
       <About />
       <Footer />
           </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,10 +1,13 @@
+import "./header.css"
+import { Link } from 'react-router-dom';
+
 function Header() {
     return (
-        <header>
-            <p>NewsExplorer</p>
-            <nav>
-    <a>Home</a>
-    <a>Sign in</a>
+        <header className="header">
+            <p className="header__title">NewsExplorer</p>
+            <nav className='header__nav'>
+    <Link to="/" className="header__home-btn">Home</Link>
+                <Link to="/saved-news" className="header__signin-btn">Sign in</Link>
   </nav>
         </header>
         

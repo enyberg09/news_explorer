0b4cc567433dbdfda25b7e958ac91b17a0676d78
Elissa Nyberg <esnyberg08@gmail.com>
2025-10-24 10:03:24 -0400

    api fix

--- src/Api/newsApi.jsx
+++ src/Api/newsApi.jsx
@@ -1,4 +1,6 @@
-const NEWS_API_BASE_URL = "https://newsapi.org/v2/everything";
+const NEWS_API_BASE_URL = import.meta.env.PROD 
+  ? "https://nomoreparties.co/news/v2/everything"
+  : "https://newsapi.org/v2/everything";
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 
 /**
